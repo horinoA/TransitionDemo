7d16967202ec9a43f4f73dd862603c036860d33e
horinoA <horinoakiko@gmail.com>
2015-10-02 15:09:42 +0900

    Initial Commit

---
+++ TransitionDemo/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  TransitionDemo
+//
+//  Created by 堀野朗子 on 2015/10/02.
+//  Copyright © 2015年 Example. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ TransitionDemoTests/TransitionDemoTests.swift
@@ -0,0 +1,36 @@
+//
+//  TransitionDemoTests.swift
+//  TransitionDemoTests
+//
+//  Created by 堀野朗子 on 2015/10/02.
+//  Copyright © 2015年 Example. All rights reserved.
+//
+
+import XCTest
+@testable import TransitionDemo
+
+class TransitionDemoTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
